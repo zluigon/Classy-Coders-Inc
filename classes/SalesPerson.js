718ecb445910e989f83024c9238741f7dd5c8c1f
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-11 13:02:31 -0400

    Update: SalesPerson class complete w/ passing tests

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,23 @@
-class SalesPerson {
+const { Employees } = require("./Employees");
 
+class SalesPerson extends Employees {
+  #totalSales;
 
+  constructor(name, title, salary, clients) {
+    super(name, title, salary);
+    this.clients = clients;
+    this.#totalSales = 0;
+  }
 
-    
+  getSalesNumber() {
+    return this.#totalSales;
+  }
+
+  makeSale(amount) {
+    this.#totalSales += amount;
+  }
 }
 
 module.exports = {
-    SalesPerson,
-}
+  SalesPerson,
+};
