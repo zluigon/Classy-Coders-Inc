2e6f21e47cc5911f14f817f8820ce837800549bf
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-11 12:58:52 -0400

    Update: SWE class complete w/ passing tests

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,10 +1,22 @@
-class SoftwareEngineer {
+const { Employees } = require("./Employees");
 
+class SoftwareEngineer extends Employees {
+  #programmingLanguages;
 
+  constructor(name, title, salary, programmingLanguages) {
+    super(name, title, salary);
+    this.#programmingLanguages = programmingLanguages;
+  }
 
-    
+  getProgrammingLanguages() {
+    return this.#programmingLanguages;
+  }
+
+  setProgrammingLanguages(language) {
+    this.#programmingLanguages.push(language);
+  }
 }
 
 module.exports = {
-    SoftwareEngineer,
-}
+  SoftwareEngineer,
+};
