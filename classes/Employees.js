89107cd924301d5b69c6890da90dc882e90721fa
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-11 12:44:48 -0400

    Update: Employees class complete w/ passing tests

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,10 +1,35 @@
 class Employees {
+  #salary;
+  #isHired;
 
+  constructor(name, title, salary) {
+    this.name = name;
+    this.title = title;
+    this.#salary = salary;
+    this.#isHired = true;
+  }
 
+  getSalary() {
+    return this.#salary;
+  }
 
-    
+  setSalary(amount) {
+    this.#salary = amount;
+  }
+
+  getStatus() {
+    return this.#isHired;
+  }
+
+  setStatus(cmd) {
+    if (cmd === "hire") {
+      this.#isHired = true;
+    } else if (cmd === "fire") {
+      this.#isHired = false;
+    }
+  }
 }
 
 module.exports = {
-    Employees,
-}
+  Employees,
+};
